0fbb81d7d3bba7ecee599154a2c58172477647c6
Narendra B T <narendrabt93@gmail.com>
2025-11-23 21:20:03 -0500

    updated location view model file

--- app/src/main/java/com/bt/quiz2/LocationViewModel.kt
+++ app/src/main/java/com/bt/quiz2/LocationViewModel.kt
@@ -1,4 +1,35 @@
 package com.bt.quiz2
 
-class LocationViewModel {
+import android.util.Log
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class LocationViewModel: ViewModel() {
+    private val _location = mutableStateOf<LocationData?>(null)
+    val location: State<LocationData?> = _location
+
+    private val _address = mutableStateOf(listOf<GeocodingResults>())
+    val address : State<List<GeocodingResults>> = _address
+
+    fun updateLocation(newLocation : LocationData){
+        _location.value = newLocation
+    }
+
+    fun fetchAddress(latlng:String) {
+        try {
+            viewModelScope.launch {
+                val result = RetrofitClient.create().getAddressFromCoordinates(
+                    latlng,
+                    BuildConfig.MAPS_API_KEY
+                )
+                _address.value =result.results
+                //Log.d("err1", " ${_address.value} ")
+            }
+        } catch (e:Exception){
+            Log.d("err1", " ${e.cause} ${e.message}")
+        }
+    }
 }
