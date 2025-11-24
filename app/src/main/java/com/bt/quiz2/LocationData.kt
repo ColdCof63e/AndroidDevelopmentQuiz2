3a0aa52e25e30021977e6fcd0d7145898a0b5032
Narendra B T <narendrabt93@gmail.com>
2025-11-23 21:21:12 -0500

    added and updated location data file

---
+++ app/src/main/java/com/bt/quiz2/LocationData.kt
@@ -0,0 +1,15 @@
+package com.bt.quiz2
+
+data class LocationData(
+    val latitude: Double,
+    val longitude: Double
+)
+
+data class GeocodingResponse(
+    val results: List<GeocodingResults>,
+    val status: String
+)
+
+data class GeocodingResults(
+    val formatted_address: String
+)
